9a3e348c5960c549f321d558ffd4d71f646cedd8
Epyonn <pdavisnguyen1@outlook.com>
2024-02-15 12:56:38 -0800

    Updated Splash Component

--- src/components/splash/Intro.tsx
+++ src/components/splash/Intro.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Typography, Box } from '@mui/material';
+import 
+import '../../App.css';
+
 
 const Intro = () => {
     return(
-        <>
-            <Typography variant="h2" sx={{ fontWeight: 'bold'}} > Hi, I'm Davis </Typography>
-            <Typography variant="h4"> Web Developer </Typography>
-        </>
+        <div className='introduction-text'>
+            <Typography variant="h2" sx={{fontWeight: 'bold'}} > Hi, I'm Davis </Typography>
+            <Typography variant="h4" sx={{fontFamily: 'BDOGrotesk'}}>Software Engineer </Typography>
+        </div>
     )
 }
 
