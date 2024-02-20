78ceeb21c2521c593a2d7660aab5b039bafe496b
Epyonn <pdavisnguyen1@outlook.com>
2024-02-20 11:42:42 -0800

    Add Github Icon

--- src/components/header/Icons.tsx
+++ src/components/header/Icons.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/Github';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Icons:React.FC = () => {
     return (
@@ -9,6 +9,11 @@ const Icons:React.FC = () => {
             <Link href="https://www.linkedin.com/in/davis-p-nguyen/">
                 <LinkedInIcon 
                     style={{ color: '#0e76a8', marginLeft: 'auto', width: '50px', height: '50px'}}
+                />
+            </Link>
+            <Link href="https://github.com/epyonn">
+                <GitHubIcon
+                    style={{ color: 'black', marginLeft: 'auto', width: '40px', height: '40px'}}
                 />
             </Link>
       
