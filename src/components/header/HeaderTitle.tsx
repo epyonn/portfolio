79b55b627643afabd691f64760e8989541ed505b
Epyonn <pdavisnguyen1@outlook.com>
2024-02-20 16:03:48 -0800

    Update About Section

--- src/components/header/HeaderTitle.tsx
+++ src/components/header/HeaderTitle.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Typography } from '@mui/material';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/Github';
+
 
 interface HeaderTitleProps {
     title: string;
