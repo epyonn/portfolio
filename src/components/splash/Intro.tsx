50d6544d811371eba7d1261bb600162afc58fa63
Epyonn <pdavisnguyen1@outlook.com>
2024-02-21 12:23:08 -0800

    Update Splash Page Responsiveness

--- src/components/splash/Intro.tsx
+++ src/components/splash/Intro.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Typography, Box } from '@mui/material';
 import { TypewriterEffectSmooth } from './typewriter-effect';
 
@@ -18,10 +18,27 @@ const Intro = () => {
         }
     ];
 
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth < 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+    })
+
     return(
         <div className='introduction-text'>
             <TypewriterEffectSmooth words={words} />
-            <Typography variant="h4"> Software Engineer </Typography>
+            {!isSmallScreen && (
+                <Typography variant='h4'> Software Engineer </Typography>
+            )}
+            {isSmallScreen && (
+                <Typography variant='body2'> Software Engineer </Typography>
+            )}
+
         </div>
     )
 }
