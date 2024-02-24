7dd5024163caf81d4ca4e3576357662cfd3a56ab
Epyonn <pdavisnguyen1@outlook.com>
2024-02-24 14:18:16 -0800

    Update Work

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { Typography } from '@mui/material';
 
+const fadeInVariants = {
+    initial: {opacity: 0},
+    animate: {
+        opacity: 1,
+        transition: { duration: 3 }
+    }
+}
 
 const Work: React.FC = () => {
     return (
         <div>
-            
+        <motion.div 
+            initial='initial'
+            animate='animate'
+            variants={fadeInVariants}
+        >
+            <Typography className="intro-text"> Work Experience </Typography>
+
+        </motion.div>
         </div>
     )
 }
