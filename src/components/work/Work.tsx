cc10e284e2680af5421cd05f73941c74fa3e4e1b
Epyonn <pdavisnguyen1@outlook.com>
2024-02-24 23:53:00 -0800

    Add Brixely Demo

--- src/components/work/Work.tsx
+++ src/components/work/Work.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Typography } from '@mui/material';
+import ProjectVideo from '../projects/ProjectVideo';
+import brixely from '../../assets/projects/brixely.mp4';
 import '../../styles/Work.css';
 
 const fadeInVariants = {
-    initial: {opacity: 0},
+    initial: { opacity: 0 },
     animate: {
         opacity: 1,
         transition: { duration: 3 }
@@ -14,14 +16,14 @@ const fadeInVariants = {
 const Work: React.FC = () => {
     return (
         <div className="work-container">
-        <motion.div 
-            initial='initial'
-            animate='animate'
-            variants={fadeInVariants}
-        >
-            <Typography className="work-text"> Work Experience </Typography>
-
-        </motion.div>
+            <motion.div
+                initial='initial'
+                animate='animate'
+                variants={fadeInVariants}
+            >
+                <Typography className="work-text"> Work Experience </Typography>
+            </motion.div>
+            <video src={brixely} autoPlay loop muted width={"850"} height={"600"} className="project-gif"/>
         </div>
     )
 }
